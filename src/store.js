00d27ed43fa2c3651378a5fdb2199090465fe5fd
znzmektm2 <tingcber2@naver.com>
2020-05-04 18:45:59 +0900

    미들웨어 적용

--- src/store.js
+++ src/store.js
@@ -1,6 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
+//import loggerMiddleware from './lib/loggerMiddleware';
 
-const store = createStore(modules)
+import { createLogger } from 'redux-logger';
+
+/* 로그 미들웨어를 만들 때 설정을 커스터마이징 할 수 있다.
+  https://github.com/evgenyrodionov/redux-logger#options
+*/
+
+const logger = createLogger();
+
+// 미들웨어가 여러 개일 때는 파라미터로 전달하면 됨. 예: applyMiddleware(a,b,c)
+// 미들웨어 순서는 여기에서 전달한 파라미터 순서대로 지정한다.
+const store = createStore(modules, applyMiddleware(logger))
 
 export default store;
